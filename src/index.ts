1fd7853c028a803596aa944a6dba9a4411a17267
Heet Vakharia <heetkv@gmail.com>
2022-12-12 08:28:27 +0530

    Added Set Input

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,8 @@ const db = pool.promise();
 
 app.post('/upload', upload.single('file'), async (req, res) => {
 	const file = req.file;
-	const { subject, exam, examType, course, code, year, semester } = req.query;
+	const { subject, exam, examType, course, code, year, semester, set } =
+		req.query;
 	if (!code) {
 		return res.status(401).json({ message: 'No code' });
 	}
@@ -42,7 +43,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 	}
 	const url = await uploadToS3(file.path, file.originalname);
 	await db.query(
-		`INSERT INTO papers (id,subject, exam, exam_type, course, url,year,semester) VALUES ('${uuid()}', '${subject}', '${exam}', '${examType}', '${course}', '${url}','${year}','${semester}')`,
+		`INSERT INTO papers (id,subject, exam, exam_type, course, url,year,semester,paper_set) VALUES ('${uuid()}', '${subject}', '${exam}', '${examType}', '${course}', '${url}','${year}','${semester}','${set}')`,
 	);
 	res.json({
 		message: 'File uploaded successfully',
